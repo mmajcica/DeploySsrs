069232830ddbc6144a7a8edee1074fa3f23b003c
Ben Sykes <ben@sykes-family.net>
2019-03-05 22:00:37 -0500

    Updates to convert true/false to boolean and adding Json version of the ReportConfiguration file

--- task/ps_modules/ssrs.psm1
+++ task/ps_modules/ssrs.psm1
@@ -499,7 +499,7 @@ function GetJsonFolderItems($Folder, [Folder]$Parent = $null)
 
     foreach($dataSet in $folder.dataSets)
     {
-        $ds = [DataSet]::new($dataSet.name, $dataSet.fileName, $dataSet.inheritParentSecurity, $dataSet.hidden)
+        $ds = [DataSet]::new($dataSet.name, $dataSet.fileName, $dataSet.inheritParentSecurity, [System.Convert]::ToBoolean($dataSet.hidden))
 
         foreach($group in $dataSet.security)
         {
@@ -511,7 +511,7 @@ function GetJsonFolderItems($Folder, [Folder]$Parent = $null)
 
     foreach($report in $folder.reports)
     {
-        $r = [Report]::new($report.name, $report.fileName, $report.inheritParentSecurity, $report.hidden)
+        $r = [Report]::new($report.name, $report.fileName, $report.inheritParentSecurity, [System.Convert]::ToBoolean($report.hidden))
 
         foreach($group in $report.security)
         {
